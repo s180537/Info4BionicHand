786bf34bc3bb65cec9f805caa0870a14c513145c
chiara2709 <s190579@th-ab.de>
2021-07-06 17:09:19 +0200

    push

---
+++ BionicHand_BETA_2/Arduino_Code.ino/Arduino_Code.ino.ino
@@ -0,0 +1,28 @@
+//Global Variable
+uint16_t dataSensor1, dataSensor2, dataSensor3, dataSensor4;
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(A0, OUTPUT);
+    pinMode(A1, OUTPUT);
+    pinMode(A2, OUTPUT);
+    pinMode(A3, OUTPUT);
+    dataSensor1 = 1;
+    dataSensor2 = 1;
+    dataSensor3 = 1;
+    dataSensor4 = 1;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    dataSensor1 = analogRead(A0);
+    dataSensor2 = analogRead(A1);
+    dataSensor3 = analogRead(A2);
+    dataSensor4 = analogRead(A3);
+    
+
+    Serial.print((String)dataSensor1 + "A" + dataSensor2 + "B" + dataSensor3 + "C" + dataSensor4 + "D"  + "\n");
+
+    delay(100);
+}
